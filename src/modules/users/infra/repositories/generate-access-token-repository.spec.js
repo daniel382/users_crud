dc458985c0427134fd879a64a6a20ce0322b8f32
Daniel Lucas <daniellucas382@gmail.com>
2021-03-11 16:44:35 -0300

    move GenerateAccessTokenRepository to its own file

--- src/modules/users/infra/repositories/generate-access-token-repository.spec.js
+++ src/modules/users/infra/repositories/generate-access-token-repository.spec.js
@@ -1,34 +1,9 @@
 const jwt = require('../../../../../__mocks__/jsonwebtoken')
 
+const GenerateAccessTokenRepository = require('./generate-access-token-repository')
 const MissingParamError = require('../../../../utils/presentation/errors/missing-param-error')
 const InvalidParamError = require('../../../../utils/presentation/errors/invalid-param-error')
 const config = require('../../../../config/secret.json')
-
-class GenerateAccessTokenRepository {
-  constructor (tokenGenerator) {
-    this.tokenGenerator = tokenGenerator
-  }
-
-  async sign (data) {
-    if (!data) {
-      throw new MissingParamError('data')
-    }
-
-    if (!this.tokenGenerator) {
-      throw new MissingParamError('TokenGenerator')
-    }
-
-    if (!this.tokenGenerator.sign) {
-      throw new InvalidParamError('TokenGenerator')
-    }
-
-    const SEVEN_DAYS = 1000 * 60 * 60 * 24 * 7
-
-    return this.tokenGenerator.sign(data, config.secret, {
-      expiresIn: SEVEN_DAYS
-    })
-  }
-}
 
 function makeSut () {
   const sut = new GenerateAccessTokenRepository(jwt)
