92ef66f81ba5e9afe811548dff5ea0107407bbaa
Daniel Lucas <daniellucas382@gmail.com>
2021-03-11 19:41:50 -0300

    create route GET: /users/:id

--- src/modules/users/main/routes/index.js
+++ src/modules/users/main/routes/index.js
@@ -4,7 +4,9 @@ const router = Router()
 const ExpressAdapter = require('../../../../utils/adapters/express-adapter')
 
 const CreateUserRouterComposer = require('../composer/create-user-router-composer')
+const ListUserByIdComposer = require('../composer/list-user-by-id-composer')
 
 router.post('/users', ExpressAdapter.adapt(CreateUserRouterComposer.compose()))
+router.get('/users/:id', ExpressAdapter.adapt(ListUserByIdComposer.compose()))
 
 module.exports = router
