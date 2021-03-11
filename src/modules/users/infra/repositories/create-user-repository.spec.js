1be0cb23f9833b21cb063570faa8ba87c164c8d0
Daniel Lucas <daniellucas382@gmail.com>
2021-03-11 13:47:24 -0300

    ensure CreateUserRepository throws if no userModel is provided

--- src/modules/users/infra/repositories/create-user-repository.spec.js
+++ src/modules/users/infra/repositories/create-user-repository.spec.js
@@ -2,7 +2,13 @@ const MissingParamError = require('../../presentation/errors/missing-param-error
 
 class CreateUserRepository {
   async save (user) {
-    throw new MissingParamError('user')
+    if (!user) {
+      throw new MissingParamError('user')
+    }
+
+    if (!this.userModel) {
+      throw new MissingParamError('UserModel')
+    }
   }
 }
 
@@ -17,5 +23,18 @@ describe('CreateUserRepository', function () {
     const promise = sut.save()
 
     expect(promise).rejects.toThrow(new MissingParamError('user'))
+  })
+
+  it('should throw if no userModel is provided', function () {
+    const { sut } = makeSut()
+    const user = {
+      name: 'any_name',
+      email: 'any@email.com',
+      password: 'any_password'
+    }
+
+    const promise = sut.save(user)
+
+    expect(promise).rejects.toThrow(new MissingParamError('UserModel'))
   })
 })
