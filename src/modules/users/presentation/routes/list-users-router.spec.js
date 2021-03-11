5f99e8fc7dd0349e86ea5087d4362326629f24bf
Daniel Lucas <daniellucas382@gmail.com>
2021-03-11 20:37:47 -0300

    ensure ListUsersRouter returns an empty list if no user is found

--- src/modules/users/presentation/routes/list-users-router.spec.js
+++ src/modules/users/presentation/routes/list-users-router.spec.js
@@ -1,6 +1,10 @@
 const HttpResponse = require('../../../../utils/presentation/helpers/http-response')
 
 class ListUsersRouter {
+  constructor (listUsersRepository) {
+    this.listUsersRepository = listUsersRepository
+  }
+
   async route (httpRequest) {
     if (!this.listUsersRepository) {
       return HttpResponse.serverError()
@@ -9,7 +13,27 @@ class ListUsersRouter {
     if (!this.listUsersRepository.list) {
       return HttpResponse.serverError()
     }
+
+    return HttpResponse.ok([])
   }
+}
+
+function makeSut () {
+  const listUsersRepositorySpy = makeListUsersRepositorySpy()
+
+  const sut = new ListUsersRouter(listUsersRepositorySpy)
+  return { sut, listUsersRepositorySpy }
+}
+
+function makeListUsersRepositorySpy () {
+  class ListUsersRepositorySpy {
+    async list () {
+
+    }
+  }
+
+  const listUsersRepositorySpy = new ListUsersRepositorySpy()
+  return listUsersRepositorySpy
 }
 
 describe('ListUsersRouter', function () {
@@ -25,5 +49,12 @@ describe('ListUsersRouter', function () {
 
     const httpResponse = await sut.route()
     expect(httpResponse.statusCode).toBe(500)
+  })
+
+  it('should return an empty list if no user is found', async function () {
+    const { sut } = makeSut()
+
+    const httpResponse = await sut.route()
+    expect(httpResponse.body).toEqual([])
   })
 })
