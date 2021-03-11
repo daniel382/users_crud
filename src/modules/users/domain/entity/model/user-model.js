999a5c8b64c8286d5c84c4e62a17eec37277cb73
Daniel Lucas <daniellucas382@gmail.com>
2021-03-11 12:58:13 -0300

    create user model

---
+++ src/modules/users/domain/entity/model/user-model.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require('mongoose')
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false
+  }
+})
+
+const userModel = model('user', userSchema)
+module.exports = userModel
