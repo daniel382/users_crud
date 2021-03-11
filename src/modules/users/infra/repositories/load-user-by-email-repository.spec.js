b21a316390d94e52a094d330c1375bb47d559898
Daniel Lucas <daniellucas382@gmail.com>
2021-03-11 13:04:05 -0300

    ensure LoadUserByEmailRepository returns an user if it exists

--- src/modules/users/infra/repositories/load-user-by-email-repository.spec.js
+++ src/modules/users/infra/repositories/load-user-by-email-repository.spec.js
@@ -1,19 +1,63 @@
+const mongoose = require('mongoose')
+
+const userModel = require('../../domain/entity/model/user-model')
+
 class LoadUserByEmailRepository {
+  constructor (userModel) {
+    this.userModel = userModel
+  }
+
   async load (email) {
-    return null
+    const result = await this.userModel.find({ email })
+    const user = result[0]
+
+    return user || null
   }
 }
 
 function makeSut () {
-  const sut = new LoadUserByEmailRepository()
+  const sut = new LoadUserByEmailRepository(userModel)
   return { sut }
 }
 
 describe('LoadUserByEmailRepository', function () {
+  let connection
+
+  beforeAll(async function () {
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true
+    })
+
+    connection = conn.connection
+  })
+
+  afterAll(async function () {
+    await connection.close()
+  })
+
+  beforeEach(async function () {
+    await userModel.deleteMany()
+  })
+
   it('should return null if no user is found', async function () {
     const { sut } = makeSut()
     const result = await sut.load('no_registered@email.com')
 
     expect(result).toBe(null)
+  })
+
+  it('should return an user if it exists', async function () {
+    const { sut } = makeSut()
+    await userModel.create({
+      name: 'any_name',
+      email: 'any@email.com',
+      password: 'any_password'
+    })
+
+    const result = await sut.load('any@email.com')
+
+    expect(result.email).toBe('any@email.com')
   })
 })
