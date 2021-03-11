a65b72031d3c281c0601f7487106aa4f86604ea4
Daniel Lucas <daniellucas382@gmail.com>
2021-03-11 08:18:17 -0300

    moving ComparePasswordUseCase to its own file

---
+++ src/modules/users/domain/usecases/compare-password-usecase.js
@@ -0,0 +1,17 @@
+const MissingParamError = require('../../presentation/errors/missing-param-error')
+
+class ComparePasswordUseCase {
+  compare (password, repeatPassword) {
+    if (!password) {
+      throw new MissingParamError('password')
+    }
+
+    if (!repeatPassword) {
+      throw new MissingParamError('repeatPassword')
+    }
+
+    return password === repeatPassword
+  }
+}
+
+module.exports = ComparePasswordUseCase
--- src/modules/users/domain/usecases/compare-password-usecase.spec.js
+++ src/modules/users/domain/usecases/compare-password-usecase.spec.js
@@ -1,18 +1,5 @@
+const ComparePasswordUseCase = require('./compare-password-usecase')
 const MissingParamError = require('../../presentation/errors/missing-param-error')
-
-class ComparePasswordUseCase {
-  compare (password, repeatPassword) {
-    if (!password) {
-      throw new MissingParamError('password')
-    }
-
-    if (!repeatPassword) {
-      throw new MissingParamError('repeatPassword')
-    }
-
-    return password === repeatPassword
-  }
-}
 
 function makeSut () {
   const sut = new ComparePasswordUseCase()
