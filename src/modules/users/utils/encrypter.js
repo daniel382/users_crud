268e7e048955de5d75a065b45b8dadca30e192b1
Daniel Lucas <daniellucas382@gmail.com>
2021-03-11 11:04:10 -0300

    refactoring Encrypter to its own file

---
+++ src/modules/users/utils/encrypter.js
@@ -0,0 +1,17 @@
+const MissingParamError = require('../presentation/errors/missing-param-error')
+
+class Encrypter {
+  constructor (crypter) {
+    this.crypter = crypter
+  }
+
+  async hash (password) {
+    if (!password) {
+      throw new MissingParamError('password')
+    }
+
+    return await this.crypter.hash(password, 10)
+  }
+}
+
+module.exports = Encrypter
--- src/modules/users/utils/encrypter.spec.js
+++ src/modules/users/utils/encrypter.spec.js
@@ -1,19 +1,7 @@
 const bcrypt = require('../../../../__mocks__/bcryptjs')
+
+const Encrypter = require('./encrypter')
 const MissingParamError = require('../presentation/errors/missing-param-error')
-
-class Encrypter {
-  constructor (crypter) {
-    this.crypter = crypter
-  }
-
-  async hash (password) {
-    if (!password) {
-      throw new MissingParamError('password')
-    }
-
-    return await this.crypter.hash(password, 10)
-  }
-}
 
 function makeSut () {
   const sut = new Encrypter(bcrypt)
