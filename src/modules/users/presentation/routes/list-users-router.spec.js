1c90df7edf82b9fc6003f3d2883d1ff2782acddc
Daniel Lucas <daniellucas382@gmail.com>
2021-03-11 20:32:30 -0300

    ensure ListUsersRouter returns 500 if an invalid ListUsersRepository is provided

--- src/modules/users/presentation/routes/list-users-router.spec.js
+++ src/modules/users/presentation/routes/list-users-router.spec.js
@@ -2,13 +2,26 @@ const HttpResponse = require('../../../../utils/presentation/helpers/http-respon
 
 class ListUsersRouter {
   async route (httpRequest) {
-    return HttpResponse.serverError()
+    if (!this.listUsersRepository) {
+      return HttpResponse.serverError()
+    }
+
+    if (!this.listUsersRepository.list) {
+      return HttpResponse.serverError()
+    }
   }
 }
 
 describe('ListUsersRouter', function () {
   it('should return 500 if no ListUsersRepository is provided', async function () {
     const sut = new ListUsersRouter()
+
+    const httpResponse = await sut.route()
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  it('should return 500 if an invalid ListUsersRepository is provided', async function () {
+    const sut = new ListUsersRouter({})
 
     const httpResponse = await sut.route()
     expect(httpResponse.statusCode).toBe(500)
