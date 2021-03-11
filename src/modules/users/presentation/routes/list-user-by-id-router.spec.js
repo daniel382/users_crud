241932eeaee17f3ee13fb57c046d49816e70aea0
Daniel Lucas <daniellucas382@gmail.com>
2021-03-11 18:54:18 -0300

    ensure ListUsersRouter returns an empty list if no user is found

--- src/modules/users/presentation/routes/list-user-by-id-router.spec.js
+++ src/modules/users/presentation/routes/list-user-by-id-router.spec.js
@@ -1,6 +1,14 @@
+const MissingParamError = require('../../../../utils/presentation/errors/missing-param-error')
+const HttpResponse = require('../../../../utils/presentation/helpers/http-response')
+
 class ListUserByIdRouter {
   async route (httpRequest) {
-    return { statusCode: 400 }
+    const { params: { id } } = httpRequest
+    if (!id) {
+      return HttpResponse.badRequest(new MissingParamError('id'))
+    }
+
+    return HttpResponse.ok([])
   }
 }
 
@@ -12,10 +20,26 @@ function makeSut () {
 describe('ListUsersRouter', function () {
   it('shoud return 400 if no id param is provided', async function () {
     const { sut } = makeSut()
-    const httpRequest = {}
+    const httpRequest = {
+      params: {}
+    }
 
     const httpResponse = await sut.route(httpRequest)
 
     expect(httpResponse.statusCode).toBe(400)
+  })
+
+  it('shoud return an empty list if no user is found', async function () {
+    const { sut } = makeSut()
+    const httpRequest = {
+      params: {
+        id: 'no_registered_id'
+      }
+    }
+
+    const httpResponse = await sut.route(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual([])
   })
 })
