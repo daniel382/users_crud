490b26d579c4a8b8702291b8bd59eef869d02b5c
Daniel Lucas <daniellucas382@gmail.com>
2021-03-11 22:28:13 -0300

    ensure DeleteUserRouter returns 500 if an invalid DeleteUserRepository is provided

--- src/modules/users/presentation/routes/delete-user-router.spec.js
+++ src/modules/users/presentation/routes/delete-user-router.spec.js
@@ -1,14 +1,31 @@
 const HttpResponse = require('../../../../utils/presentation/helpers/http-response')
 
 class DeleteUserRouter {
+  constructor (deleteUserRepository) {
+    this.deleteUserRepository = deleteUserRepository
+  }
+
   async route () {
-    return HttpResponse.serverError()
+    if (!this.deleteUserRepository) {
+      return HttpResponse.serverError()
+    }
+
+    if (!this.deleteUserRepository.delete) {
+      return HttpResponse.serverError()
+    }
   }
 }
 
 describe('DeleteUserRouter', function () {
   it('should return 500 if no DeleteUserRepository is provided', async function () {
     const sut = new DeleteUserRouter()
+    const httpResponse = await sut.route()
+
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  it('should return 500 if an invalid DeleteUserRepository is provided', async function () {
+    const sut = new DeleteUserRouter({})
     const httpResponse = await sut.route()
 
     expect(httpResponse.statusCode).toBe(500)
