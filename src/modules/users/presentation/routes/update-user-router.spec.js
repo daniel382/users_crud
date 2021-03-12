f2c54a9b79f080b2c4157bdb69e6c76b6bdc09e3
Daniel Lucas <daniellucas382@gmail.com>
2021-03-12 12:47:23 -0300

    ensure UpdateUserRouter returns 500 if an invalid LoadUserByIdRepository is provided

--- src/modules/users/presentation/routes/update-user-router.spec.js
+++ src/modules/users/presentation/routes/update-user-router.spec.js
@@ -1,12 +1,29 @@
 class UpdateUserRouter {
+  constructor (loadUserByIdRepository) {
+    this.loadUserByIdRepository = loadUserByIdRepository
+  }
+
   async route () {
-    return { statusCode: 500 }
+    if (!this.loadUserByIdRepository) {
+      return { statusCode: 500 }
+    }
+
+    if (!this.loadUserByIdRepository.load) {
+      return { statusCode: 500 }
+    }
   }
 }
 
 describe('UpdateUserRouter', function () {
   it('should return 500 if no LoadUserByIdRepository is provided', async function () {
     const sut = new UpdateUserRouter()
+    const httpResponse = await sut.route()
+
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  it('should return 500 if an invalid LoadUserByIdRepository is provided', async function () {
+    const sut = new UpdateUserRouter({})
     const httpResponse = await sut.route()
 
     expect(httpResponse.statusCode).toBe(500)
