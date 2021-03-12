c27aad2755232e8ac2b6fccdcc2e71612eb8124b
Daniel Lucas <daniellucas382@gmail.com>
2021-03-11 21:28:47 -0300

    ensure ListUserRepository throws if no page is provided

--- src/modules/users/infra/repositories/list-users-repository.spec.js
+++ src/modules/users/infra/repositories/list-users-repository.spec.js
@@ -1,11 +1,25 @@
+const userModel = require('../../domain/entity/model/user-model')
 const MissingParamError = require('../../../../utils/presentation/errors/missing-param-error')
 
 class ListUsersRepository {
+  constructor (userModel) {
+    this.userModel = userModel
+  }
+
   async list (page, limit) {
     if (!this.userModel) {
       throw new MissingParamError('UserModel')
     }
+
+    if (!page) {
+      throw new MissingParamError('page')
+    }
   }
+}
+
+function makeSut () {
+  const sut = new ListUsersRepository(userModel)
+  return { sut }
 }
 
 describe('ListUsersRepository', function () {
@@ -14,5 +28,12 @@ describe('ListUsersRepository', function () {
     const promise = sut.list()
 
     expect(promise).rejects.toThrow(new MissingParamError('UserModel'))
+  })
+
+  it('should throw if no page is provided', function () {
+    const { sut } = makeSut()
+    const promise = sut.list()
+
+    expect(promise).rejects.toThrow(new MissingParamError('page'))
   })
 })
