7bc04196f20f1ed4b5439e6fc77e0399c48849c5
Daniel Lucas <daniellucas382@gmail.com>
2021-03-12 16:58:49 -0300

    ensure LoginRouter returns 400 if no password is provided

--- src/modules/users/presentation/routes/login-router.spec.js
+++ src/modules/users/presentation/routes/login-router.spec.js
@@ -2,8 +2,14 @@ const HttpResponse = require('../../../../utils/presentation/helpers/http-respon
 const MissingParamError = require('../../../../utils/presentation/errors/missing-param-error')
 
 class LoginRouter {
-  async route () {
-    return HttpResponse.badRequest(new MissingParamError('email'))
+  async route (email, password) {
+    if (!email) {
+      return HttpResponse.badRequest(new MissingParamError('email'))
+    }
+
+    if (!password) {
+      return HttpResponse.badRequest(new MissingParamError('password'))
+    }
   }
 }
 
@@ -16,6 +22,13 @@ describe('LoginRouter', function () {
   it('should return 400 if no email is provided', async function () {
     const { sut } = makeSut()
     const httpResponse = await sut.route()
+
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  it('should return 400 if no password is provided', async function () {
+    const { sut } = makeSut()
+    const httpResponse = await sut.route('any@email.com')
 
     expect(httpResponse.statusCode).toBe(400)
   })
