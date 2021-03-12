36e2e30c63f0a7af91fd119db491033129dbe9fa
Daniel Lucas <daniellucas382@gmail.com>
2021-03-11 21:18:22 -0300

    move ListUsersRouter to its own file

--- src/modules/users/presentation/routes/list-users-router.spec.js
+++ src/modules/users/presentation/routes/list-users-router.spec.js
@@ -1,27 +1,4 @@
-const HttpResponse = require('../../../../utils/presentation/helpers/http-response')
-
-class ListUsersRouter {
-  constructor (listUsersRepository) {
-    this.listUsersRepository = listUsersRepository
-  }
-
-  async route (httpRequest) {
-    if (!this.listUsersRepository) {
-      return HttpResponse.serverError()
-    }
-
-    if (!this.listUsersRepository.list) {
-      return HttpResponse.serverError()
-    }
-
-    const page = (httpRequest.query.page - 1) || 0
-    const limit = (httpRequest.query.limit) || 10
-
-    const users = await this.listUsersRepository.list(page, limit)
-
-    return HttpResponse.ok(users)
-  }
-}
+const ListUsersRouter = require('./list-users-router')
 
 function makeSut () {
   const listUsersRepositorySpy = makeListUsersRepositorySpy()
