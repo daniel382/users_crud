8fd8741ddd7667c5066a8332f5c8fa3883d6ab5f
Daniel Lucas <daniellucas382@gmail.com>
2021-03-12 12:23:07 -0300

    ensure DeleteUserRepository returns true when user is successfully deleted

--- src/modules/users/infra/repositories/delete-user-repository.spec.js
+++ src/modules/users/infra/repositories/delete-user-repository.spec.js
@@ -1,4 +1,6 @@
 const MissingParamError = require('../../../../utils/presentation/errors/missing-param-error')
+const userModel = require('../../domain/entity/model/user-model')
+const mongoHelper = require('../../../../lib/database')
 
 class DeleteUserRepository {
   constructor (userModel) {
@@ -13,15 +15,29 @@ class DeleteUserRepository {
     if (!this.userModel) {
       throw new MissingParamError('UserModel')
     }
+
+    return Boolean(await this.userModel.findByIdAndDelete(id))
   }
 }
 
 function makeSut () {
-  const sut = new DeleteUserRepository()
+  const sut = new DeleteUserRepository(userModel)
   return { sut }
 }
 
 describe('DeleteUserRepository', function () {
+  beforeAll(async function () {
+    await mongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async function () {
+    await mongoHelper.disconnect()
+  })
+
+  beforeEach(async function () {
+    await userModel.deleteMany()
+  })
+
   it('should throw if no id is provided', function () {
     const { sut } = makeSut()
     const promise = sut.delete()
@@ -34,5 +50,17 @@ describe('DeleteUserRepository', function () {
     const promise = sut.delete('any_user_id')
 
     expect(promise).rejects.toThrow(new MissingParamError('UserModel'))
+  })
+
+  it('should return true when user is successfully deleted', async function () {
+    const { sut } = makeSut()
+    const user = await userModel.create({
+      name: 'name',
+      email: 'email',
+      password: 'password'
+    })
+
+    const result = await sut.delete(user._id)
+    expect(result).toBe(true)
   })
 })
