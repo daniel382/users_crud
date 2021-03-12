35ea0be2b35a5c3ffeec1fee14f93ed81bbdecdf
Daniel Lucas <daniellucas382@gmail.com>
2021-03-12 12:51:58 -0300

    ensure UpdateUserRouter returns 500 if no UpdateUserByIdRepository is provided

--- src/modules/users/presentation/routes/update-user-router.spec.js
+++ src/modules/users/presentation/routes/update-user-router.spec.js
@@ -1,17 +1,39 @@
+const HttpResponse = require('../../../../utils/presentation/helpers/http-response')
+
 class UpdateUserRouter {
-  constructor (loadUserByIdRepository) {
+  constructor (loadUserByIdRepository, updateUserByIdRepository) {
     this.loadUserByIdRepository = loadUserByIdRepository
+    this.updateUserByIdRepository = updateUserByIdRepository
   }
 
   async route () {
     if (!this.loadUserByIdRepository) {
-      return { statusCode: 500 }
+      return HttpResponse.serverError()
     }
 
     if (!this.loadUserByIdRepository.load) {
-      return { statusCode: 500 }
+      return HttpResponse.serverError()
+    }
+
+    if (!this.updateUserByIdRepository) {
+      return HttpResponse.serverError()
     }
   }
+}
+
+function makeLoadUserByIdRepositorySpy () {
+  class LoadUserByIdRepositorySpy {
+    async load (id) {
+      this.id = id
+      return this.user
+    }
+  }
+
+  const loadUserByIdRepositorySpy = new LoadUserByIdRepositorySpy()
+  loadUserByIdRepositorySpy.user = {
+    _id: 'any_id'
+  }
+  return loadUserByIdRepositorySpy
 }
 
 describe('UpdateUserRouter', function () {
@@ -24,6 +46,14 @@ describe('UpdateUserRouter', function () {
 
   it('should return 500 if an invalid LoadUserByIdRepository is provided', async function () {
     const sut = new UpdateUserRouter({})
+    const httpResponse = await sut.route()
+
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  it('should return 500 if no UpdateUserByIdRepository is provided', async function () {
+    const loadUserByIdRepositorySpy = makeLoadUserByIdRepositorySpy()
+    const sut = new UpdateUserRouter(loadUserByIdRepositorySpy)
     const httpResponse = await sut.route()
 
     expect(httpResponse.statusCode).toBe(500)
