adffada3a2956336ded519da91978546896bd979
Daniel Lucas <daniellucas382@gmail.com>
2021-03-12 18:19:48 -0300

    ensure UpdateAccessTokenRepository throws if no user model is provided

--- src/modules/users/infra/repositories/update-access-token-repository.spec.js
+++ src/modules/users/infra/repositories/update-access-token-repository.spec.js
@@ -1,9 +1,17 @@
 const MissingParamError = require('../../../../utils/presentation/errors/missing-param-error')
 
 class UpdateAccessTokenRepository {
+  constructor (userModel) {
+    this.userModel = userModel
+  }
+
   async update (accessToken) {
     if (!accessToken) {
       throw new MissingParamError('AccessToken')
+    }
+
+    if (!this.userModel) {
+      throw new MissingParamError('UserModel')
     }
   }
 }
@@ -14,5 +22,12 @@ describe('UpdateAccessTokenRepository', function () {
     const promise = sut.update()
 
     expect(promise).rejects.toThrow(new MissingParamError('AccessToken'))
+  })
+
+  it('shound throw if no userModel is provided', function () {
+    const sut = new UpdateAccessTokenRepository()
+    const promise = sut.update('any_accessToken')
+
+    expect(promise).rejects.toThrow(new MissingParamError('UserModel'))
   })
 })
