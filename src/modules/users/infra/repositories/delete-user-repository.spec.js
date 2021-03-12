dd1b93937f7df58b9826906b14a07d0982d12eac
Daniel Lucas <daniellucas382@gmail.com>
2021-03-12 11:59:41 -0300

    ensure DeleteUserRepository throws if no userModel is provided

--- src/modules/users/infra/repositories/delete-user-repository.spec.js
+++ src/modules/users/infra/repositories/delete-user-repository.spec.js
@@ -1,8 +1,18 @@
 const MissingParamError = require('../../../../utils/presentation/errors/missing-param-error')
 
 class DeleteUserRepository {
+  constructor (userModel) {
+    this.userModel = userModel
+  }
+
   async delete (id) {
-    throw new MissingParamError('id')
+    if (!id) {
+      throw new MissingParamError('id')
+    }
+
+    if (!this.userModel) {
+      throw new MissingParamError('UserModel')
+    }
   }
 }
 
@@ -17,5 +27,12 @@ describe('DeleteUserRepository', function () {
     const promise = sut.delete()
 
     expect(promise).rejects.toThrow(new MissingParamError('id'))
+  })
+
+  it('should throw if no userModel is provided', function () {
+    const sut = new DeleteUserRepository()
+    const promise = sut.delete('any_user_id')
+
+    expect(promise).rejects.toThrow(new MissingParamError('UserModel'))
   })
 })
