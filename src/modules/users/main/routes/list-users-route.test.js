2832a504e800a28b75d13e9cf151d3fd28adf304
Daniel Lucas <daniellucas382@gmail.com>
2021-03-11 22:16:34 -0300

    ensure ListUsersRoute returns a list of users

---
+++ src/modules/users/main/routes/list-users-route.test.js
@@ -0,0 +1,41 @@
+const request = require('supertest')
+
+const app = require('../../../../lib/app')
+const mongoHelper = require('../../../../lib/database')
+const userModel = require('../../domain/entity/model/user-model')
+
+describe('ListUsersRoute', function () {
+  const fakeUsers = [{
+    name: 'user01',
+    email: 'user01@email.com',
+    password: 'user01_password'
+  }, {
+    name: 'user02',
+    email: 'user02@email.com',
+    password: 'user02_password'
+  }, {
+    name: 'user03',
+    email: 'user03@email.com',
+    password: 'user03_password'
+  }]
+
+  beforeAll(async function () {
+    await mongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async function () {
+    await mongoHelper.disconnect()
+  })
+
+  beforeEach(async function () {
+    await userModel.deleteMany()
+  })
+
+  it('should return a list of users', async function () {
+    await userModel.insertMany(fakeUsers)
+    const httpResponse = await request(app)
+      .get('/users')
+
+    expect(httpResponse.body.length).toBe(3)
+  })
+})
