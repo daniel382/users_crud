7b076f3a9952f7b1f4aa2a6d38f61af193d28d13
Daniel Lucas <daniellucas382@gmail.com>
2021-03-12 17:57:01 -0300

    ensure Decrypter throws if no hash is provided

--- src/utils/infra/decrypter.spec.js
+++ src/utils/infra/decrypter.spec.js
@@ -3,7 +3,13 @@ const MissingParamError = require('../presentation/errors/missing-param-error')
 
 class Decrypter {
   async compare (password, hash) {
-    throw new MissingParamError('password')
+    if (!password) {
+      throw new MissingParamError('password')
+    }
+
+    if (!hash) {
+      throw new MissingParamError('hash')
+    }
   }
 }
 
@@ -18,5 +24,12 @@ describe('Decrypter', function () {
     const promise = sut.compare()
 
     expect(promise).rejects.toThrow(new MissingParamError('password'))
+  })
+
+  it('should throw if no hash is provided', function () {
+    const { sut } = makeSut()
+    const promise = sut.compare('any_password')
+
+    expect(promise).rejects.toThrow(new MissingParamError('hash'))
   })
 })
