7ce30eaa91c1cd7d11953e053bb87a64733d009e
Daniel Lucas <daniellucas382@gmail.com>
2021-03-12 18:22:28 -0300

    ensure UpdateAccessTokenRepository throws if no id is provided

--- src/modules/users/infra/repositories/update-access-token-repository.spec.js
+++ src/modules/users/infra/repositories/update-access-token-repository.spec.js
@@ -5,7 +5,11 @@ class UpdateAccessTokenRepository {
     this.userModel = userModel
   }
 
-  async update (accessToken) {
+  async update (id, accessToken) {
+    if (!id) {
+      throw new MissingParamError('id')
+    }
+
     if (!accessToken) {
       throw new MissingParamError('AccessToken')
     }
@@ -17,16 +21,23 @@ class UpdateAccessTokenRepository {
 }
 
 describe('UpdateAccessTokenRepository', function () {
-  it('shound throw if no accessToken is provided', function () {
+  it('shound throw if no id is provided', function () {
     const sut = new UpdateAccessTokenRepository()
     const promise = sut.update()
+
+    expect(promise).rejects.toThrow(new MissingParamError('id'))
+  })
+
+  it('shound throw if no accessToken is provided', function () {
+    const sut = new UpdateAccessTokenRepository()
+    const promise = sut.update('any_id')
 
     expect(promise).rejects.toThrow(new MissingParamError('AccessToken'))
   })
 
   it('shound throw if no userModel is provided', function () {
     const sut = new UpdateAccessTokenRepository()
-    const promise = sut.update('any_accessToken')
+    const promise = sut.update('any_id', 'any_accessToken')
 
     expect(promise).rejects.toThrow(new MissingParamError('UserModel'))
   })
